ce2a5ddc1510e441a909d131b00013a1ddab2a8c
Bernardo <moonbe77@gmail.com>
2020-05-29 17:54:23 +1000

    Updates

---
+++ precache-manifest.b8aba44b4deda3566e9169747894a138.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45c0145a1096502a3b3666397a4f751",
+    "url": "/moonbe77.github.io/FM_testimonials_slider/index.html"
+  },
+  {
+    "revision": "bc234af802d5a4f6d8e2",
+    "url": "/moonbe77.github.io/FM_testimonials_slider/static/js/2.536a2056.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/moonbe77.github.io/FM_testimonials_slider/static/js/2.536a2056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f918eb62328c43e3b53",
+    "url": "/moonbe77.github.io/FM_testimonials_slider/static/js/main.d5dd0c77.chunk.js"
+  },
+  {
+    "revision": "1ed4a674b6148fdc1f40",
+    "url": "/moonbe77.github.io/FM_testimonials_slider/static/js/runtime-main.d2d25ff7.js"
+  }
+]);
