a18756b3f8b9fe80b19c3482b110dc41ba0202b7
Bernardo <moonbe77@gmail.com>
2021-01-27 17:39:42 +1100

    Updates

---
+++ precache-manifest.175a7b4a255ac746d6be27f111108cdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289d3bfbf6129cf80fda573105fe5ca7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d601c3c68295fe6a80ea",
+    "url": "./static/js/2.5f00f95b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.5f00f95b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d77b9943fb5b45275d8e",
+    "url": "./static/js/main.a8932aa1.chunk.js"
+  },
+  {
+    "revision": "05b38a3648a362c2a896",
+    "url": "./static/js/runtime-main.ac255a3c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/moonbe77.github.io/FM_testimonials_slider/precache-manifest.c00245e84f58297bd6f241bd29443991.js"
+  "./precache-manifest.175a7b4a255ac746d6be27f111108cdd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moonbe77.github.io/FM_testimonials_slider/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
